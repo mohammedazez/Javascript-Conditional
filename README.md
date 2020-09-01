c13369f8e33b25abcc7d3baa39ee62d5905acbf1
azezmohammed <muhamadaziz047@gmail.com>
2020-09-01 22:50:50 +0700

    Javascript Conditional

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Javascript-Conditional
+
+https://www.w3schools.com/js/js_if_else.asp JS Conditions
+https://www.w3schools.com/js/js_switch.asp JS Switch
+https://www.w3schools.com/js/js_comparisons.asp JS comparison
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator Conditional (ternary) operator
---
+++ learn/js-conditional.js
@@ -0,0 +1,34 @@
+// //  Kondisi jika benar
+// if (true) {
+//   console.log("Pesan ini akan di print");
+// }
+// //  Kondisi jika salah
+// if (false) {
+//   console.log("Pesan ini tidak sakan di print");
+// }
+
+// let lapar = true;
+// if (lapar) {
+//   console.log("yuk makan");
+// }
+
+// let nilai = 9;
+// if (nilai === 10) {
+//   console.log("variable nilai yang disimpan adalah benar 10");
+// }
+
+// let lapar = false;
+// if (lapar) {
+//   console.log("Yuk Makan");
+// } else {
+//   console.log("Tidak makan");
+// }
+
+// let cekLampu = "hijau";
+// if (cekLampu === "merah") {
+//   console.log("Berhenti !");
+// } else if (cekLampu === "kuning") {
+//   console.log("Jalan Pelan!");
+// } else if (cekLampu === "hijau") {
+//   console.log("Silahkan jalan");
+// }
